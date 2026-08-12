760b13008db0903d17d97385543e6c510a2cc790
André Brandão <andrebrandao@MacBook-Pro-de-Andre.local>
2026-08-12 16:35:49 -0300

    Criação da estrutura da pilha

--- pilha.h
+++ pilha.h
@@ -3,6 +3,15 @@
 
 #include <stdio.h>
 
+#define TAM_MAX 1000
+
+typedef int TipoItem;   // define o tipo de dados da pilha
+
+// Estrutura Da Pilha
+typedef struct {
+    TipoItem vet[TAM_MAX];
+    int topo;
+} Pilha;
 
 
 #endif /* pilha_h */
